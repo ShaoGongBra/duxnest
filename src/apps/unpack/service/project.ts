8eabdb4575d47298670de42e882dfde957acea77
xujian <908634674@qq.com>
2022-03-04 17:57:03 +0800

    上传

--- src/apps/unpack/service/project.ts
+++ src/apps/unpack/service/project.ts
@@ -11,6 +11,11 @@ import { execSync, exec } from 'child_process';
 import { join } from 'path';
 import config from '../config/project';
 
+const OS = {
+  android: 'android',
+  ios: 'ios',
+};
+
 @Injectable()
 export class ProjectService {
   /**
@@ -97,9 +102,6 @@ export class ProjectService {
       ? config.rootDir.split(':')[0] + ': && ' + dir
       : dir;
   }
-  commenExec(name: string) {
-    return this.cdExec(name) + 'git pull && yarn && ';
-  }
   projectExec(name: string) {
     const packageJson = JSON.parse(
       readFileSync(join(config.rootDir, name, 'package.json'), {
@@ -117,64 +119,136 @@ export class ProjectService {
     const status = (global.unpackStatus = global.unpackStatus || {});
     if (name) {
       status[name] = status[name] || {
-        android: { status: '' },
-        ios: { status: '' },
+        android: { status: '', log: [] },
+        ios: { status: '', log: [] },
       };
       return status[name];
     }
     return status;
   }
-  android(name: string) {
-    if (!this.isProject(name)) {
-      throw new HttpException(name + ' 不是一个合法项目', 500);
-    }
+  log(log = '') {
+    return Date.now() + ': ' + log;
+  }
+  startActive(name: string, os: string, log = '') {
     const status = this.getStatus(name);
-    if (status.android.status === 'active') {
+    if (status[os].status === 'active') {
       throw new HttpException(name + ' 正在打包', 500);
     }
-    status.android.status = 'active';
-    exec(`${this.commenExec(name)}${this.projectExec(name)}`, (error) => {
-      if (error) {
-        status.android.status = 'error';
-        status.android.message = error;
-      } else {
-        // 上传到蒲公英
-        const dir = join(
-          'android',
-          'app',
-          'build',
-          'outputs',
-          'apk',
-          'release',
-        );
-        let fileName = '';
-        if (existsSync(join(config.rootDir, name, dir, 'app-release.apk'))) {
-          fileName = join(dir, 'app-release.apk');
-        } else if (
-          existsSync(
-            join(config.rootDir, name, dir, 'app-armeabi-v7a-release.apk'),
-          )
-        ) {
-          fileName = join(dir, 'app-armeabi-v7a-release.apk');
+    status[os].status = 'active';
+    if (log) {
+      status[os].log.push(this.log(log));
+    }
+  }
+  endActive(name: string, os: string, log = '') {
+    const status = this.getStatus(name);
+    status[os].status = 'complete';
+    if (log) {
+      status[os].log.push(this.log(log));
+    }
+  }
+  errorActive(name: string, os: string, err: any) {
+    const status = this.getStatus(name);
+    status[os].status = 'error';
+    status[os].message = err?.message || err;
+  }
+  exec(...cmds: string[]) {
+    return new Promise((resolve, reject) => {
+      const callback = (prevInfo = '') => {
+        if (!cmds.length) {
+          resolve(prevInfo);
+          return;
         }
-        if (!fileName) {
-          status.android.status = 'error';
-          status.android.message = '找不到打包后的文件';
-        } else {
-          exec(
-            this.cdExec(name) + 'yarn duxapp rn pgyer ' + fileName,
-            (error) => {
-              if (error) {
-                status.android.status = 'error';
-                status.android.message = error;
-              } else {
-                status.android.status = 'complete';
-                status.android.completeTime = Date.now();
-              }
-            },
-          );
-        }
-      }
+        const item = cmds.shift();
+        exec(item, (error, info) => {
+          if (error) {
+            reject(error);
+          } else {
+            callback(info);
+          }
+        });
+      };
+      callback();
     });
+  }
+  async base(name: string, os: string) {
+    this.startActive(name, os, '开始代码同步');
+    await this.exec(this.cdExec(name) + 'git pull && yarn');
+    this.endActive(name, os);
+  }
+  async buildCode(name: string, os: string) {
+    this.startActive(name, os, '开始编译项目');
+    await this.exec(`${this.cdExec(name)}yarn build:rn`);
+    this.endActive(name, os);
+  }
+  async build(name: string, os: string) {
+    const packageJson = JSON.parse(
+      readFileSync(join(config.rootDir, name, 'package.json'), {
+        encoding: 'utf8',
+      }),
+    );
+    let buildExec: string;
+    // 判断项目类型 执行不同的命令
+    if (packageJson.dependencies['@tarojs/taro'].startsWith('2.')) {
+      buildExec = `cd android && gradlew assembleRelease`;
+    } else {
+      buildExec = `yarn build:${os}${
+        os === OS.ios ? ' && yarn export:ios' : ''
+      }`;
+    }
+    this.startActive(name, os, '开始打包安装包');
+    await this.exec(`${this.cdExec(name)}${buildExec}`);
+    this.endActive(name, os);
+  }
+  async codepush(name: string, os: string) {
+    this.startActive(name, os, '开始上传热更新代码');
+    await this.exec(`${this.cdExec(name)}yarn codepush:${os}`);
+    this.endActive(name, os);
+  }
+  async prger(name: string, os: string) {
+    this.startActive(name, os, '开始上传蒲公英');
+    // 上传到蒲公英
+    const dir = join('android', 'app', 'build', 'outputs', 'apk', 'release');
+    let fileName = '';
+    if (existsSync(join(config.rootDir, name, dir, 'app-release.apk'))) {
+      fileName = join(dir, 'app-release.apk');
+    } else if (
+      existsSync(join(config.rootDir, name, dir, 'app-armeabi-v7a-release.apk'))
+    ) {
+      fileName = join(dir, 'app-armeabi-v7a-release.apk');
+    }
+    if (!fileName) {
+      throw '找不到打包后的文件';
+    }
+    await this.exec(this.cdExec(name) + 'yarn duxapp rn pgyer ' + fileName);
+    this.endActive(name, os);
+  }
+  async upload(name: string, os: string) {
+    this.startActive(name, os, '开始上传蒲公英');
+    await this.exec(this.cdExec(name) + 'yarn upload-ios');
+    this.endActive(name, os);
+  }
+  async android(name: string) {
+    try {
+      await this.base(name, OS.android);
+      await this.buildCode(name, OS.android);
+      await this.codepush(name, OS.android);
+      await this.build(name, OS.android);
+      await this.prger(name, OS.android);
+      this.endActive(name, OS.android, '打包完成 已上传至蒲公英');
+    } catch (error) {
+      this.errorActive(name, OS.android, error);
+    }
+  }
+  async ios(name: string) {
+    try {
+      await this.base(name, OS.ios);
+      await this.buildCode(name, OS.ios);
+      await this.codepush(name, OS.ios);
+      await this.build(name, OS.ios);
+      await this.upload(name, OS.ios);
+      this.endActive(name, OS.ios, '打包完成 已上传至应用商店');
+    } catch (error) {
+      this.errorActive(name, OS.ios, error);
+    }
   }
 }
